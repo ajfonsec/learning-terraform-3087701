64afa49bc95c3ce0c44719dfee74de8f78aca1c8
ajfonsec <92533398+ajfonsec@users.noreply.github.com>
2025-04-24 16:35:02 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-  subnet_id = subnet-02979c360edee0ea5
+  subnet_id = "subnet-02979c360edee0ea5"
 
   tags = {
     Name = "HelloWorld"
